4d2195217c9f034f18ebd1f6690f4fd1a2dd3214
Joao Augusto da Cruz <joao_cruz02@hotmail.com>
2018-04-27 14:30:50 -0300

    Adicionado timer para renovação diaria

--- renovador.py
+++ renovador.py
@@ -26,44 +26,47 @@ login = {
 login['login'] = sys.argv[1]
 login['pwd'] = sys.argv[2]
 
-#inicia a sessão
-s = requests.Session()
-#faz a resquisição post com os dadosdo login
-r = s.post('http://www.dibib.ufsj.edu.br/cgi-bin/wxis.exe', data=login)
-#requisição para a parte
-soap = BeautifulSoup(r.text,'html')
-listLinks = soap.find_all('a',href = True)
-link1 = str(listLinks[1])
+
+while True:
+    #inicia a sessão
+    s = requests.Session()
+    #faz a resquisição post com os dadosdo login
+    r = s.post('http://www.dibib.ufsj.edu.br/cgi-bin/wxis.exe', data=login)
+    #requisição para a parte
+    soap = BeautifulSoup(r.text,'html')
+    listLinks = soap.find_all('a',href = True)
+    link1 = str(listLinks[1])
 
 
 
 
-#localizando o tmp file e realizando seu parse
+    #localizando o tmp file e realizando seu parse
 
-link1.find('tmp=')
-tmpFile = str(link1[56:].split()[0])
-tmpFile = tmpFile[:len(tmpFile)-1]
+    link1.find('tmp=')
+    tmpFile = str(link1[56:].split()[0])
+    tmpFile = tmpFile[:len(tmpFile)-1]
 
-#requisicao para o segundo link do dibib
-link2request = 'http://www.dibib.ufsj.edu.br/cgi-bin/wxis.exe?IsisScript=phl82/017.xis&'+tmpFile
-r = s.get(link2request)
+    #requisicao para o segundo link do dibib
+    link2request = 'http://www.dibib.ufsj.edu.br/cgi-bin/wxis.exe?IsisScript=phl82/017.xis&'+tmpFile
+    r = s.get(link2request)
 
-# localizando todos os codigos dos livros alugados
+    # localizando todos os codigos dos livros alugados
 
-soap = BeautifulSoup(r.text,'html')
-listLinks = soap.find_all('a',href = True)
+    soap = BeautifulSoup(r.text,'html')
+    listLinks = soap.find_all('a',href = True)
 
-index = len(listLinks)
-del(listLinks[index-1])
+    index = len(listLinks)
+    del(listLinks[index-1])
 
-response = []
-#realizando a requisicao para cada livro alugado
-for element in listLinks:
-    string = str(element).split(';')[1]
-    bookCode = string.split('&')[0]
-    response.append(s.get('http://www.dibib.ufsj.edu.br/cgi-bin/wxis.exe?IsisScript=phl82/037.xis&' + bookCode +'&acv=001&'+tmpFile))
+    response = []
+    #realizando a requisicao para cada livro alugado
+    for element in listLinks:
+        string = str(element).split(';')[1]
+        bookCode = string.split('&')[0]
+        response.append(s.get('http://www.dibib.ufsj.edu.br/cgi-bin/wxis.exe?IsisScript=phl82/037.xis&' + bookCode +'&acv=001&'+tmpFile))
 
-#print resposta do servidor
-for element in response:
-    print(element.text)
+    #print resposta do servidor
+    for element in response:
+        print(element.text)
 
+    time.sleep(24*3600)
